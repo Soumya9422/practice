c2cc99b271c5bb0022f9fc656716bce5e1b6ccfd
Soumya9422 <mfsi.soumyadas@gmail.com>
2023-02-01 12:44:17 +0530

    create a demo

--- src/test/sum.java
+++ src/test/sum.java
@@ -7,12 +7,12 @@ public class sum {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Sum of 2 number
-		int a , b , c;
-		Scanner sc = new Scanner(System.in);   
-		System.out.println("Enter the 1st number to add :");
-		a = sc.nextInt();
-		System.out.println("Enter the 2nd number to add :");
-		b = sc.nextInt(); 
+		int a = 10, b =20 , c =0;
+		//Scanner sc = new Scanner(System.in);   
+		//System.out.println("Enter the 1st number to add :");
+		//a = sc.nextInt();
+		//System.out.println("Enter the 2nd number to add :");
+		//b = sc.nextInt(); 
 		c = a + b;
 		System.out.println ("Sum of 2 number is :"+c);
 	}
